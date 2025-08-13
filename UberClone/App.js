6a9ab7c493658f5e52bb312d9ec7288f49888336
Sameer003-Imt <sameerimtiaz003@gmail.com>
2025-08-14 00:02:07 +0500

    Add UberClone/App.js 1755111728814

---
+++ UberClone/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import MainNavigator from './navigation/MainNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MainNavigator />
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
